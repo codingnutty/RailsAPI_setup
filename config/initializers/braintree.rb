ee3c1d3bca9e7832dccc9d657c04db10f4da9680
Utsab Saha <utsab.k.saha@gmail.com>
2015-10-25 06:15:23 -0700

    removing braintree.rb, adding it to .gitignore

--- config/initializers/braintree.rb
+++
@@ -1,5 +0,0 @@
-Braintree::Configuration.environment = :sandbox
-Braintree::Configuration.merchant_id = "h6jbvytp9gssvtv6"
-Braintree::Configuration.public_key = "366z8d4rshwy6y2v"
-Braintree::Configuration.private_key = "804e1fbf076daa7f28179cbd58e99a63"
-
