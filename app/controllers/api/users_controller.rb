69e1699ad23d1e7cc2bbb07023ed3765eb5fc9b0
codingnutty <nruthy2687@gmail.com>
2015-10-25 06:04:08 -0700

    braintree stuff

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,18 +1,37 @@
 class Api::UsersController < ApplicationController
   def index
     p "Hello"
+    @client_token = generate_client_token
     render :action => :index
-    generate_client_token
 
     #put paypal here
     # ping paypal, paste in their code
     # p out stuff to verify that it's working
   end
 
+  def new
+    @client_token = generate_client_token
+  end
+
   def create
     p "In create"
     p "params: #{params}"
+
+
+    # @result =  Braintree::Transaction.sale(
+    #   amount: "0.005",
+    #   payment_method_nonce: params[:payment_method_nonce],
+    #               customer: {
+    #                 # first_name: params[:first_name],
+    #                 # last_name: params[:last_name],
+    #                 email: params[:email],
+    #               },
+    #               options: {
+    #                 store_in_vault: true
+    #               })
   end
+
+
 
   private
   def generate_client_token
